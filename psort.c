4a0315216d58ea8c8d82dd9f3abbb794a7e8d206
Maya Knotts <knotts@royal-10.cs.wisc.edu>
2023-04-06 21:06:03 -0500

    attempt at mergesort - still debugging

--- psort.c
+++ psort.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <string.h>
 
-
+int num_records;
 
 //Struct for key(first 4 bytes) and value(next 96 bytes)
 typedef struct{
@@ -10,31 +11,62 @@ typedef struct{
 	char value[97];  //1 extra bit for NULL terminating char
 } Record;
 
+// Struct for list of Records
+typedef struct{
+	Record *records;
+	int size;
+} Records;
+
+Records partitionRecords(Records rs, int first, int last){
+	int size = (last - first) + 1;
+	Record *new_rs = (Record*) malloc(size * sizeof(Record));
+	for (int i = 0; i < size; i++){
+		new_rs[i] = rs.records[first+i];
+	}
+	Records r = {new_rs, size};
+	return r;
+}
 
 //function to merge both halves
-void merge(Record *arr, int left, int mid, int right){
-
-
+Records merge(Records a, Records b){
+	Record* m = (Record*) malloc(num_records * sizeof(Record));
+	int i_a = 0;
+	int i_b = 0;
+	int m_i = 0;
+	while (i_a < a.size && i_b < b.size){
+		if (strcmp(a.records[i_a].key, b.records[i_b].key) <= 0){
+			Record r = { a.records[i_a].key, a.records[i_a++].value};
+			m[m_i++] = r;
+		} else {
+			Record r = { b.records[i_b].key, b.records[i_b++].value};
+			m[m_i++] = r;
+		}
+	}
+	Records merged_r = {m, m_i++};
+	return merged_r;
 }
 
 //recursive function that splits array into two halves and calls itself on each half
 //https://www.geeksforgeeks.org/c-program-for-merge-sort/
-void merge_sort_helper(Record *arr, int left, int right){
+Records merge_sort(Records rs){
+	int left = 0;
+	int right = rs.size - 1;
 	if(right < left){
 		int mid = left + (right-left) / 2;
 
-		merge_sort_helper(arr, left, mid);
-		merge_sort_helper(arr,mid+1, right);
+		Records a = merge_sort(partitionRecords(rs, left, mid));
+		Records b = merge_sort(partitionRecords(rs, mid+1, right));
 
-		merge(arr, left, mid, right);
-	}
+		return merge(a, b);
+	} 
+	return rs;
 
 }
 
 
-void merge_sort(Record *arr, int num_records){
-	merge_sort_helper(arr,0,num_records-1);
-}
+// void merge_sort(Records rs){
+// 	merge_sort_helper(rs);
+// }
 
 
 int main(int argc, char** argv) {
@@ -56,17 +88,17 @@ int main(int argc, char** argv) {
 		exit(EXIT_FAILURE);
 	}
 
-  // Get the size of the input file
-  fseek(input_file, 0, SEEK_END);
-  int input_file_size = ftell(input_file);
+	// Get the size of the input file
+	fseek(input_file, 0, SEEK_END);
+	int input_file_size = ftell(input_file);
 
 
-  // Set the file pointer back to the beginning
-  fseek(input_file, 0, SEEK_SET);
+	// Set the file pointer back to the beginning
+	fseek(input_file, 0, SEEK_SET);
 
 	// Calculate the number of records in the file
 	//int num_records = input_file_size / sizeof(Record);
-	int num_records = (input_file_size + sizeof(Record) - 1) / sizeof(Record);
+	num_records = (input_file_size + sizeof(Record) - 1) / sizeof(Record);
 
 	//printf("Records: %d\n",num_records);
 
@@ -97,10 +129,12 @@ int main(int argc, char** argv) {
 	//Record 2: key=cccc, value=yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
 	//Record 3: key=bbbb, value=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
+	Records rs = {records, num_records};
 
-	merge_sort(records, num_records); //FIX MERGE FUNCTION 
-
-
+	Records res = merge_sort(rs); //FIX MERGE FUNCTION 
+	for (int i = 0; i < res.size; i++){
+		printf("Record %d: key=%s, value=%s\n", i+1, res.records[i].key, res.records[i].value);
+	}
 
 	free(records);
 
